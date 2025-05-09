3cbaa27c2fe1aa68e3f6a0d263590e2d52212089
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-05-09 14:05:52 +0900

    20250508

---
+++ 10THYMELEAF/src/main/java/com/example/demo/C01OpenData/OpenDataController.java
@@ -0,0 +1,12 @@
+package com.example.demo.C01OpenData;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/openData")
+public class OpenDataController {
+
+}
